585a0be49d128d0bf94c79f452f459a5bc5f0fa5
iaihrs <146690632+iaihrs@users.noreply.github.com>
2023-10-13 13:47:59 +0100

    Add project files.

---
+++ Main.cs
@@ -0,0 +1,23 @@
+﻿using Loadson;
+using LoadsonAPI;
+using System.Reflection;
+using System.Xml.Schema;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+namespace Jumpson
+{
+    internal class Main : Mod
+    {
+        public override void Update(float deltaTime)
+        {
+            if (Input.GetKeyDown(KeyCode.Space))
+            {
+                Type jump = typeof(PlayerMovement);
+                FieldInfo jumpForce = jump.GetField("jumpForce", BindingFlags.NonPublic | BindingFlags.Instance);
+                jumpForce.SetValue(PlayerMovement.Instance, 1500);
+            }
+        }
+    }
+}
